e2f546970ebd452c2888d6e575d6d96b0c1ec5a7
nataug62 <nataug62@gmail.com>
2020-11-22 09:48:17 -0500

    basic chat functionality working

--- control/comms.py
+++ control/comms.py
@@ -1,9 +1,9 @@
+import socket
+
 SERVER_PORT = 8080
 
 if __name__ == "__main__":
 	print('Starting program...')
-
-	quit()
 
 	# create an IPv4 TCP server socket and set it up to listen
 	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,3 +14,13 @@ if __name__ == "__main__":
 	(clientsocket, address) = serversocket.accept()
 
 	print('Client connected')
+
+	# echo messages back to the sender
+	while True:
+		data = clientsocket.recv(4096)
+		if not data:
+			break
+		print(data)
+		data_s = b'ECHO: ' + data + b'\0'
+		print(data_s)
+		clientsocket.sendall(data_s)
--- remote/comms.c
+++ remote/comms.c
@@ -1,3 +1,5 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS // TODO - work around this
+
 #include<stdio.h> // standard input/output
 #include<winsock2.h> // windows socket header
 
@@ -11,6 +13,7 @@ int main(int argc, char* argv[]) {
 	SOCKET sock;
 	struct sockaddr_in server;
 	int retCode;
+	char buffer[4096];
 
 	// initialize winsock
 	retCode = WSAStartup(MAKEWORD(2, 2), &wsa);
@@ -36,6 +39,30 @@ int main(int argc, char* argv[]) {
 	}
 
 	printf("Connected\n");
+
+	// get input and send it, then wait for a response
+	while (1 == 1) {
+		if (fgets(buffer, 4096, stdin) == NULL) {
+			printf("Error when reading user input! Exiting program...\n");
+			return 1;
+		}
+		retCode = send(sock, buffer, (int) strlen(buffer), 0);
+		if (retCode == SOCKET_ERROR) {
+			printf("Send failed with error: %d\n", WSAGetLastError());
+			return 1;
+		}
+		retCode = recv(sock, buffer, 4096, 0);
+		if (retCode > 0) {
+			printf(buffer);
+		}
+		else if (retCode == 0) {
+			printf("Connection closed...\n");
+			return 0;
+		}
+		else {
+			printf("Recv failed with error: %d\n", WSAGetLastError());
+		}
+	}
 
 	return 0;
 }
