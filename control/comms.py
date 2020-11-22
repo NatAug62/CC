afef6ab2b60b46221c9d4c5dfd036792180b5149
NatAug62 <nataug62@gmail.com>
2020-11-22 04:22:44 -0500

    why

---
+++ control/comms.py
@@ -0,0 +1,16 @@
+SERVER_PORT = 8080
+
+if __name__ == "__main__":
+	print('Starting program...')
+
+	quit()
+
+	# create an IPv4 TCP server socket and set it up to listen
+	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	serversocket.bind(('', SERVER_PORT))
+	serversocket.listen(5)
+
+	# listen for incoming connections and accept them
+	(clientsocket, address) = serversocket.accept()
+
+	print('Client connected')
